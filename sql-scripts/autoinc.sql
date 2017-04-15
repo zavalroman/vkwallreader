f210fb8c9f7647fb3a7280bccabeb4f25356a6a3
Roman Zavalishin <zavalroman@gmail.com>
2017-04-15 23:12:07 +0500

    re part ok

--- sql-scripts/autoinc.sql
+++ sql-scripts/autoinc.sql
@@ -38,6 +38,7 @@ END^
 SET TERM ; ^
 
 
+
 CREATE GENERATOR vkphotoInc;
 
 SET TERM ^ ;
@@ -49,6 +50,7 @@ BEGIN
     new.ID = gen_id (vkphotoInc, 1);
 END^
 SET TERM ; ^
+
 
 CREATE GENERATOR commentsInc;
 
@@ -62,6 +64,7 @@ BEGIN
 END^
 SET TERM ; ^
 
+
 CREATE GENERATOR vkdocInc;
 
 SET TERM ^ ;
@@ -71,5 +74,83 @@ AS
 BEGIN
     if (new.ID is null) then
     new.ID = gen_id (vkdocInc, 1);
+END^
+SET TERM ; ^
+
+
+CREATE GENERATOR eventtypeInc;
+
+SET TERM ^ ;
+CREATE TRIGGER eventtypeInc FOR eventtype ACTIVE
+BEFORE INSERT POSITION 1
+AS
+BEGIN
+    if (new.ID is null) then
+    new.ID = gen_id (eventtypeInc, 1);
+END^
+SET TERM ; ^
+
+
+CREATE GENERATOR eventInc;
+
+SET TERM ^ ;
+CREATE TRIGGER eventInc FOR event ACTIVE
+BEFORE INSERT POSITION 1
+AS
+BEGIN
+    if (new.ID is null) then
+    new.ID = gen_id (eventInc, 1);
+END^
+SET TERM ; ^
+
+
+CREATE GENERATOR circleInc;
+
+SET TERM ^ ;
+CREATE TRIGGER circleInc FOR circle ACTIVE
+BEFORE INSERT POSITION 1
+AS
+BEGIN
+    if (new.ID is null) then
+    new.ID = gen_id (circleInc, 1);
+END^
+SET TERM ; ^
+
+
+CREATE GENERATOR albumInc;
+
+SET TERM ^ ;
+CREATE TRIGGER albumInc FOR album ACTIVE
+BEFORE INSERT POSITION 1
+AS
+BEGIN
+    if (new.ID is null) then
+    new.ID = gen_id (albumInc, 1);
+END^
+SET TERM ; ^
+
+
+CREATE GENERATOR fandomInc;
+
+SET TERM ^ ;
+CREATE TRIGGER fandomInc FOR fandom ACTIVE
+BEFORE INSERT POSITION 1
+AS
+BEGIN
+    if (new.ID is null) then
+    new.ID = gen_id (fandomInc, 1);
+END^
+SET TERM ; ^
+
+
+CREATE GENERATOR trackInc;
+
+SET TERM ^ ;
+CREATE TRIGGER trackInc FOR track ACTIVE
+BEFORE INSERT POSITION 1
+AS
+BEGIN
+    if (new.ID is null) then
+    new.ID = gen_id (trackInc, 1);
 END^
 SET TERM ; ^
